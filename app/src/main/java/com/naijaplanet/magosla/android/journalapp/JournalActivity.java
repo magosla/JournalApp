1d036cac7cc1f5978319a58016e06542e9c40806
Magnus Lator <magosla2001@gmail.com>
2018-07-01 16:46:42 +0100

    fixed issues found with JLint

--- app/src/main/java/com/naijaplanet/magosla/android/journalapp/JournalActivity.java
+++ app/src/main/java/com/naijaplanet/magosla/android/journalapp/JournalActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
