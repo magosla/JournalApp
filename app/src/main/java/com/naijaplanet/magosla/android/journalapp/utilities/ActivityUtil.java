b53076572dc1b255b6fa3b0815be000a87823d36
Magnus Lator <magosla2001@gmail.com>
2018-07-01 20:58:35 +0100

    added helper method to delete journal

--- app/src/main/java/com/naijaplanet/magosla/android/journalapp/utilities/ActivityUtil.java
+++ app/src/main/java/com/naijaplanet/magosla/android/journalapp/utilities/ActivityUtil.java
@@ -3,11 +3,14 @@ package com.naijaplanet.magosla.android.journalapp.utilities;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.transition.Explode;
 import android.view.Window;
 
@@ -17,6 +20,7 @@ import com.google.android.gms.tasks.Task;
 import com.naijaplanet.magosla.android.journalapp.EditorActivity;
 import com.naijaplanet.magosla.android.journalapp.JournalActivity;
 import com.naijaplanet.magosla.android.journalapp.MainActivity;
+import com.naijaplanet.magosla.android.journalapp.R;
 
 /**
  * This class provides helper methods for activities
@@ -26,10 +30,12 @@ public class ActivityUtil {
     private static boolean mTransitionEnabled;
     // it a call the enable activity transition has been made
     private static boolean mCalledTransitionEnable;
+
     /**
      * Starts an activity to edit or create journal
-     * @param activity the current activity
-     * //@param user the user object
+     *
+     * @param activity   the current activity
+     *                   //@param user the user object
      * @param journalKey the journal key
      */
     public static void launchEditor(Activity activity,/* User user,*/ @Nullable String journalKey) {
@@ -44,14 +50,15 @@ public class ActivityUtil {
 
     /**
      * Starts an activity to open the journal detail
-     * @param activity the current activity
-     * //@param user the user object
+     *
+     * @param activity   the current activity
+     *                   //@param user the user object
      * @param journalKey the journal key
      */
-    public static void launchJournal(Activity activity,/* User user,*/ @Nullable String journalKey){
+    public static void launchJournal(Activity activity,/* User user,*/ @Nullable String journalKey) {
         Intent journalIntent = new Intent(activity, JournalActivity.class);
         // send the user object
-       // journalIntent.putExtra(Values.USER_INTENT_DATA_KEY, user);
+        // journalIntent.putExtra(Values.USER_INTENT_DATA_KEY, user);
 
         // send the journal key
         journalIntent.putExtra(Values.EXTRA_JOURNAL_KEY, journalKey);
@@ -66,10 +73,10 @@ public class ActivityUtil {
      * @return the bundle if available
      */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private static Bundle getTransitionsBundle(Activity activity){
+    private static Bundle getTransitionsBundle(Activity activity) {
         Bundle bundle = null;
         // if enableTransition has been called and the device SDK support transition
-        if(mCalledTransitionEnable && mTransitionEnabled) {
+        if (mCalledTransitionEnable && mTransitionEnabled) {
             bundle = ActivityOptions.makeSceneTransitionAnimation(activity).toBundle();
         }
         return bundle;
@@ -77,10 +84,11 @@ public class ActivityUtil {
 
     /**
      * Enables activity transition if supported by the SDK version
+     *
      * @param activity the current activity
      */
-    public static  void enableTransition(Activity activity){
-        if(!mCalledTransitionEnable) {
+    public static void enableTransition(Activity activity) {
+        if (!mCalledTransitionEnable) {
             mCalledTransitionEnable = true;
             // Check if we're running on Android 5.0 or higher
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -93,7 +101,7 @@ public class ActivityUtil {
 
             } else {
                 // Swap without transition
-                mTransitionEnabled =false;
+                mTransitionEnabled = false;
             }
         }
 
@@ -114,5 +122,31 @@ public class ActivityUtil {
                 activity.finish();
             }
         });
+    }
+
+    /**
+     * @param context    the application context
+     * @param userId     the app user id
+     * @param journalKey the journal key
+     * @param callback   callback called when action is complete
+     */
+    public static void deleteJournal(Context context, @NonNull final String userId
+            , @NonNull final String journalKey, @Nullable final FirebaseUtil.OnCompletionListener callback) {
+
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.title_delete_confirm)
+                .setMessage(R.string.msg_delete_confirm)
+                .setNegativeButton(R.string.action_no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton(R.string.action_yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseUtil.deleteItem(userId, journalKey, callback);
+                    }
+                }).show();
     }
 }
