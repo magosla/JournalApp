22eb0645ffed1b9c5bb664c8e3d13f96d263afc2
Magnus Lator <magosla2001@gmail.com>
2018-07-01 16:47:42 +0100

    included a helper method to delete journal entry

--- app/src/main/java/com/naijaplanet/magosla/android/journalapp/utilities/FirebaseUtil.java
+++ app/src/main/java/com/naijaplanet/magosla/android/journalapp/utilities/FirebaseUtil.java
@@ -1,7 +1,11 @@
 package com.naijaplanet.magosla.android.journalapp.utilities;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 /**
@@ -13,17 +17,55 @@ public class FirebaseUtil {
 
     /**
      * Initializes the FirebaseDatabase instance
+     *
      * @return the FirebaseDatabase instance
      */
-    public static FirebaseDatabase getFirebaseDatabase(){
+    @SuppressWarnings("WeakerAccess")
+    public static FirebaseDatabase getFirebaseDatabase() {
         Log.d("FirebaseUtil", "Requesting FirebaseDatabase instance");
 
         // don't initialize if previously initialized
-        if(mDatabase == null){
+        if (mDatabase == null) {
             mDatabase = FirebaseDatabase.getInstance();
             mDatabase.setPersistenceEnabled(true);
             Log.d("FirebaseUtil", "FirebaseDatabase instance initialization");
         }
         return mDatabase;
+    }
+
+    public static DatabaseReference getJournalsRef(@NonNull String userId) {
+        return FirebaseUtil.getFirebaseDatabase().getReference()
+                .child(String.format("%s/%s",
+                        Values.JOURNAL_DATABASE_PATH, userId));
+    }
+
+    public static DatabaseReference getJournalRef(@NonNull String userId, @NonNull String journalKey) {
+        return FirebaseUtil.getFirebaseDatabase().getReference()
+                .child(String.format("%s/%s/%s",
+                        Values.JOURNAL_DATABASE_PATH, userId, journalKey));
+    }
+
+    public static void deleteItem(@NonNull String userId, @NonNull String journalKey, @Nullable final OnCompletionListener callback) {
+        getJournalRef(userId, journalKey).getRef().removeValue(new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                if (callback != null) {
+                    boolean hasError = databaseError != null && !databaseError.getMessage().isEmpty();
+                    callback.onComplete(hasError);
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public interface OnCompletionListener {
+        // just basic, no checking if it was a success or not
+
+        /**
+         * Called when the process has completed
+         *
+         * @param encounteredError if there was an error
+         */
+        void onComplete(boolean encounteredError);
     }
 }
