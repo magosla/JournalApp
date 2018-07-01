f59a9e798d060e82c882ffc612f680e5cbc776bd
Magnus Lator <magosla2001@gmail.com>
2018-07-01 21:59:03 +0100

    added helper method to delete journal

--- app/src/main/java/com/naijaplanet/magosla/android/journalapp/utilities/FirebaseUtil.java
+++ app/src/main/java/com/naijaplanet/magosla/android/journalapp/utilities/FirebaseUtil.java
@@ -33,18 +33,35 @@ public class FirebaseUtil {
         return mDatabase;
     }
 
+    /**
+     *  get the DatabaseReference for the user journals
+     * @param userId id of the app user
+     * @return Retruns the reference to the journals for the app user
+     */
     public static DatabaseReference getJournalsRef(@NonNull String userId) {
         return FirebaseUtil.getFirebaseDatabase().getReference()
                 .child(String.format("%s/%s",
                         Values.JOURNAL_DATABASE_PATH, userId));
     }
 
+    /**
+     *  get the DatabaseReference for the user selected journals
+     * @param userId id of the app user
+     * @param journalKey key that identifies the journal
+     * @return Retruns the reference to the journals for the app user
+     */
     public static DatabaseReference getJournalRef(@NonNull String userId, @NonNull String journalKey) {
         return FirebaseUtil.getFirebaseDatabase().getReference()
                 .child(String.format("%s/%s/%s",
                         Values.JOURNAL_DATABASE_PATH, userId, journalKey));
     }
 
+    /**
+     * deletes a journal entry
+     * @param userId  id of the app user
+     * @param journalKey  key that identifies the journal
+     * @param callback an optional callback to call when the process is complete
+     */
     public static void deleteItem(@NonNull String userId, @NonNull String journalKey, @Nullable final OnCompletionListener callback) {
         getJournalRef(userId, journalKey).getRef().removeValue(new DatabaseReference.CompletionListener() {
             @Override
@@ -57,6 +74,9 @@ public class FirebaseUtil {
         });
     }
 
+    /**
+     * An interface for task conpletion
+     */
     @SuppressWarnings({"WeakerAccess", "unused"})
     public interface OnCompletionListener {
         // just basic, no checking if it was a success or not
